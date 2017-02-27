b377737dd2844d25dc4cab523968602ba7df4af7
Daniel Parks <dparks715@yahoo.com>
2017-02-27 15:47:36 -0500

    Testing board class

---
+++ test_board.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require_relative 'board.rb'
+
+class TestBoard < Minitest::Test
+
+	def test_new_board
+		board = Board.new
+		assert_equal(Array.new(9, ''), board.ttt_board)
+	end
+
+	def test_update_first_position
+		board = Board.new
+		board.update_position(0, 'X')
+		assert_equal(['X', '', '', '', '', '', '', '', ''], board.ttt_board)
+	end
+
+	def test_first_x_and_second_o
+		board = Board.new
+		board.update_position(0, 'X')
+		board.update_position(1, 'O')
+		assert_equal(['X', 'O', '', '', '', '', '', '', ''], board.ttt_board)
+	
+	end
+
+end
