1f126c39203be926f6f560f5fc3101969671d01d
Daniel Parks <dparks715@yahoo.com>
2017-02-27 23:40:12 -0500

    Added initialize function to player class, used gets chomp to get user name and marker choice

--- board.rb
+++ board.rb
@@ -89,3 +89,18 @@ class Board
 
 end
 
+class Player
+
+	attr_accessor :name, :marker
+
+	def initialize
+		print 'Please enter your name: '
+		@name = gets.chomp
+
+		print 'Enter X or O to choose your marker: '
+		@marker = gets.chomp
+	end
+
+
+
+end
