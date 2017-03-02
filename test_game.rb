348e4b6c7c4dde3f48acbc8e429717a5988a5a5e
Dan <dparks715@yahoo.com>
2017-03-01 22:01:53 -0500

    Trying to test Game class initialize function using sequential and random AI as player 1 and 2, failing currently

--- test_game.rb
+++ test_game.rb
@@ -3,6 +3,12 @@ require_relative 'game.rb'
 
 class TestGame < Minitest::Test
 
-
+	def test_players
+		p1 = Sequential.new('X')
+		p2 = Random.new('O')
+		game = Game.new(p1, p2)
+		assert_equal('X', game.p1.marker)
+		
+	end
 
 end
