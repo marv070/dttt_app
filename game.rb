09ef8f8258bb4afb8341aa09ad74ca8b87785bbc
Dan <dparks715@yahoo.com>
2017-03-01 23:00:30 -0500

    Added comments for my goals/to-do and to clarify how things should flow. Updated the display board I found online to use my class names (board.ttt_board rather than just board)

--- game.rb
+++ game.rb
@@ -8,6 +8,11 @@ class Game
 
 	attr_accessor :player_1, :player_2, :board
 
+
+	# Need to add way to keep track of active player
+	# Add checks for tie and winner per game flow in my launch_game.rb
+
+
 	def initialize(player_1, player_2)
 		@player_1 = player_1
 		@player_2 = player_2	
@@ -15,11 +20,11 @@ class Game
 	end
 
 	def display_board(board)
-	  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+	  puts " #{board.ttt_board[0]} | #{board.ttt_board[1]} | #{board.ttt_board[2]} "
 	  puts "-----------"
-	  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+	  puts " #{board.ttt_board[3]} | #{board.ttt_board[4]} | #{board.ttt_board[5]} "
 	  puts "-----------"
-	  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+	  puts " #{board.ttt_board[6]} | #{board.ttt_board[7]} | #{board.ttt_board[8]} "
 	end
 
 
--- launch_game.rb
+++ launch_game.rb
@@ -7,4 +7,13 @@ require 'game.rb'
 user_input = player_1
 user_input2 = player_2
 
+# Initializes a new game with player 1 and 2 selected.
 Game.new(player_2, player_2)
+
+# Game flow will go here.
+# Need to start game
+# Display board
+# Get a move from active player
+# Update board via board.update_position
+# Check for winner or tie
+# Change players and repeat
