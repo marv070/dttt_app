d8bd42c4cfaa7af36920d538cadfd97f793d14ce
Dan <dparks715@yahoo.com>
2017-03-01 20:47:43 -0500

    Class and function done, fixed minor syntax on instantiating new class

---
+++ sequential.rb
@@ -0,0 +1,11 @@
+class Sequential
+
+	def initialize(marker)
+		@marker = marker	
+	end
+
+	def ai_choice(board_state)
+		board_state.index('')
+	end
+
+end
--- test_sequential.rb
+++ test_sequential.rb
@@ -4,8 +4,8 @@ require_relative 'sequential.rb'
 class TestSequentialAI < Minitest::Test
 
 	def test_ai_picks_first_pos
-		ai = new.Sequential('O')
-		assert_equal(0, ai.ai_choice)
+		ai = Sequential.new('O')
+		assert_equal(0, ai.ai_choice(['', '', '', '', '', '', '', '', '']))
 	end
 
 end
