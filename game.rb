2a1f9a26f2d892c102ae220ef0b5f0f967a9164d
Dan <dparks715@yahoo.com>
2017-03-01 22:10:59 -0500

    Added attr accessor, changed variable names in test file to match game.rb file, tests pass now

--- game.rb
+++ game.rb
@@ -6,6 +6,13 @@ require_relative 'random.rb'
 
 class Game
 
+	attr_accessor :player_1, :player_2
+
+	def initialize(player_1, player_2)
+		@player_1 = player_1
+		@player_2 = player_2		
+	end
+
 	def display_board(board)
 	  puts " #{board[0]} | #{board[1]} | #{board[2]} "
 	  puts "-----------"
--- test_game.rb
+++ test_game.rb
@@ -4,10 +4,10 @@ require_relative 'game.rb'
 class TestGame < Minitest::Test
 
 	def test_players
-		p1 = Sequential.new('X')
-		p2 = Random.new('O')
-		game = Game.new(p1, p2)
-		assert_equal('X', game.p1.marker)
+		player_1 = Sequential.new('X')
+		player_2 = Random.new('O')
+		game = Game.new(player_1, player_2)
+		assert_equal('X', game.player_1.marker)
 		
 	end
 
