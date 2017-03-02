d8b3d26efc54569901b90a40bd1777e9d3c25d61
Dan <dparks715@yahoo.com>
2017-03-01 20:48:44 -0500

    More tests, function seems to work

--- test_sequential.rb
+++ test_sequential.rb
@@ -8,4 +8,14 @@ class TestSequentialAI < Minitest::Test
 		assert_equal(0, ai.ai_choice(['', '', '', '', '', '', '', '', '']))
 	end
 
+	def test_ai_picks_second_pos
+		ai = Sequential.new('O')
+		assert_equal(1, ai.ai_choice(['X', '', '', '', '', '', '', '', '']))
+	end
+
+	def test_ai_picks_third_pos
+		ai = Sequential.new('O')
+		assert_equal(2, ai.ai_choice(['X', 'X', '', '', '', '', '', '', '']))
+	end
+
 end
