f9be2971bbf43b70716cdbe1a9e2788eb13c1d7b
Dan <dparks715@yahoo.com>
2017-03-01 21:54:46 -0500

    Changed test name in player class to reflect previous changes to class

--- test_player.rb
+++ test_player.rb
@@ -3,7 +3,7 @@ require_relative 'player.rb'
 
 class TestPlayer < Minitest::Test
 
-	def test_get_player_name
+	def test_get_player_move
 		player = Player.new('X')
 		assert_equal(0, player.get_move)
 	end
