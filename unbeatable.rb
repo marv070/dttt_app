6a6a9f4777dff05d159bd098934386cc561b0172
Daniel Parks <dparks715@yahoo.com>
2017-03-13 21:22:30 -0400

    Downsized win and block functions by making reusuable code its own function and calling it in win and block

--- unbeatable.rb
+++ unbeatable.rb
@@ -41,83 +41,11 @@ class UnbeatableAI
 	end
 
 	def win(ttt_board)
-
-		win_combos = [
-			[ttt_board[0], ttt_board[1], ttt_board[2]],
-			[ttt_board[3], ttt_board[4], ttt_board[5]],
-			[ttt_board[6], ttt_board[7], ttt_board[8]],
-			[ttt_board[0], ttt_board[3], ttt_board[6]],
-			[ttt_board[1], ttt_board[4], ttt_board[7]],
-			[ttt_board[2], ttt_board[5], ttt_board[8]],
-			[ttt_board[0], ttt_board[4], ttt_board[8]],
-			[ttt_board[2], ttt_board[4], ttt_board[6]]
-			
-		]
-
-		winners = [
-			[0, 1, 2],
-			[3, 4, 5],
-			[6, 7, 8],
-			[0, 3, 6],
-			[1, 4, 7],
-			[2, 5, 8],
-			[0, 4, 8],
-			[2, 4, 6]
-		]
-
-		move = 10
-		
-		win_combos.each_with_index do |val, pos|
-
-			if val.count(marker) == 2 && val.count(' ') == 1
-				results = val.index(' ')
-				move = winners[pos][results]
-			else
-				move
-			end
-		end
-		move
+		win_or_block(ttt_board, @marker)
 	end
 
 	def block(ttt_board)
-
-		win_combos = [
-			[ttt_board[0], ttt_board[1], ttt_board[2]],
-			[ttt_board[3], ttt_board[4], ttt_board[5]],
-			[ttt_board[6], ttt_board[7], ttt_board[8]],
-			[ttt_board[0], ttt_board[3], ttt_board[6]],
-			[ttt_board[1], ttt_board[4], ttt_board[7]],
-			[ttt_board[2], ttt_board[5], ttt_board[8]],
-			[ttt_board[0], ttt_board[4], ttt_board[8]],
-			[ttt_board[2], ttt_board[4], ttt_board[6]]
-			
-		]
-
-		winners = [
-			[0, 1, 2],
-			[3, 4, 5],
-			[6, 7, 8],
-			[0, 3, 6],
-			[1, 4, 7],
-			[2, 5, 8],
-			[0, 4, 8],
-			[2, 4, 6]
-		]
-
-		move = 10
-
-		win_combos.each_with_index do |val, pos|
-
-			if
-				val.count(opponent) == 2 && val.count(' ') == 1
-				results = val.index(' ')
-				move = winners[pos][results]
-			else
-				move
-			end
-		end
-		move
-
+		win_or_block(ttt_board, @opponent)
 	end
 
 	def check_for_fork(ttt_board)
@@ -313,4 +241,46 @@ class UnbeatableAI
 			opponent = 'X'
 		end
 	end
+
+	def win_or_block(ttt_board, player)
+
+		win_combos = [
+			[ttt_board[0], ttt_board[1], ttt_board[2]],
+			[ttt_board[3], ttt_board[4], ttt_board[5]],
+			[ttt_board[6], ttt_board[7], ttt_board[8]],
+			[ttt_board[0], ttt_board[3], ttt_board[6]],
+			[ttt_board[1], ttt_board[4], ttt_board[7]],
+			[ttt_board[2], ttt_board[5], ttt_board[8]],
+			[ttt_board[0], ttt_board[4], ttt_board[8]],
+			[ttt_board[2], ttt_board[4], ttt_board[6]]
+			
+		]
+
+		winners = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6]
+		]
+
+		move = 10
+
+		win_combos.each_with_index do |val, pos|
+
+			if
+				val.count(player) == 2 && val.count(' ') == 1
+				results = val.index(' ')
+				move = winners[pos][results]
+			else
+				move
+			end
+		end
+		move
+
+	end
+
 end
