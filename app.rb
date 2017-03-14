6d3611f7bf1c8e2e51e4139d0ab14d5d7a8a82a4
Daniel Parks <dparks715@yahoo.com>
2017-03-13 22:06:46 -0400

    First test setup, need to decide on new game file for app, or put logic directly in app

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
 require 'sinatra'
-require_relative 'play_console_game.rb'
+# require_relative 'TODO -CHANGE FROM CONSOLE GAME'
+require_relative 'board.rb'
+require_relative 'unbeatable.rb'
+require_relative 'player_classes.rb'
+
+#Required console_game to start, but that isn't going to work
+#Need to make new file specifically for use in app, or do the logic in the app.rb
+
 
 class TicTacToe < Sinatra::Base
+
+	get '/' do
+
+	end
 
 
 
--- tests/test_app.rb
+++ tests/test_app.rb
@@ -3,13 +3,16 @@ require 'rack/test'
 require_relative '../app.rb'
 
 class TestApp < Minitest::Test
-  include Rack::Test::Methods
+	include Rack::Test::Methods
 
-  def app
-  	TicTacToe
-  end
+	def app
+		TicTacToe
+	end
 
-  
+	def test_welcome
+		get '/'
+		assert(last_response.ok?)
+	end
 
 
- end
+end
