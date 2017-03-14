d928ee9a3ed25fc67558033366c8d81cf1180260
Daniel Parks <dparks715@yahoo.com>
2017-03-14 04:16:39 -0400

    Added form with buttons for difficulty selection, added tests

--- tests/test_app.rb
+++ tests/test_app.rb
@@ -13,6 +13,8 @@ class TestApp < Minitest::Test
 		get '/'
 		assert(last_response.ok?)
 		assert(last_response.body.include?('Welcome to Tic Tac Toe'))
+		assert(last_response.body.include?("<input type='submit' name='easy' value='Easy'>"))
+		assert(last_response.body.include?("<form action='player1' method='post'>"))
 	end
 
 
